39bd50e0636bf65d3514b8cd7a6ae7de59b8e916
phi1ip83 <philip.dossantos@hotmail.com>
2023-06-06 17:20:38 -0400

    init

---
+++ src/main/java/com/example/testmod/TestModMain.java
@@ -0,0 +1,38 @@
+package com.example.testmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TestModMain.MODID)
+public class TestModMain
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "testmod";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+
+    public TestModMain()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+
+        modEventBus.addListener(this::commonSetup);
+
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+}
