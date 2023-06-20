a5e6c0194d5cbd7a7aca55069c9938a8643b01cf
phi1ip83 <philip.dossantos@hotmail.com>
2023-06-19 22:27:14 -0400

    forgot item drop when broken. oops.

--- src/main/java/com/example/testmod/BlockInit.java
+++ src/main/java/com/example/testmod/BlockInit.java
@@ -16,7 +16,7 @@ import java.util.function.Supplier;
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TestModMain.MODID);
 
-    public static final RegistryObject<Block> ENDER_BLOCK = registerBlock("ender_block.json" , ()->new Block(BlockBehaviour.Properties.of(Material.STONE)));
+    public static final RegistryObject<Block> ENDER_BLOCK = registerBlock("ender_block" , ()->new Block(BlockBehaviour.Properties.of(Material.STONE)));
 
 
 
@@ -29,5 +29,6 @@ public class BlockInit {
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block){
         return ItemInit.ITEMS.register(name, ()-> new BlockItem(block.get(), new Item.Properties().tab(ItemInit.TAB)));
     }
+
 
 }
